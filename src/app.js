2c0d31d3e40aa68746b670e8d622315ea67025ba
Jose Octavio de Castro Neves Jr <jose@verifymyage.co.uk>
2021-05-01 18:35:16 -0300

    do we need context/Dockerfile?

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ app.put('/stress/:elemento/tempostress/:tempoStress/intervalo/:intervalo/ciclos/
 
 app.use('/api/produto', product);
 
-var developer_db_url = 'mongodb://mongouser:mongopwd@localhost:27017/admin';
+var developer_db_url = 'mongodb://localhost:27017/admin';
 var mongoUrl = process.env.MONGODB_URI || developer_db_url;
 
 mongoose.Promise = global.Promise;
